b0e1cc0713d05643a522cfd8e18f27211ae0241f
GIGABAIT93 <59637348+GIGABAIT93@users.noreply.github.com>
2025-01-14 10:48:40 +0200

    Update config.php

--- Config/config.php
+++ Config/config.php
@@ -5,6 +5,6 @@ return [
     'name' => 'GatewayPack',
     'icon' => 'https://ibb.co/Mn9dDRt',
     'author' => 'GIGABAIT',
-    'version' => '1.0.1',
+    'version' => '1.0.2',
     'wemx_version' => '>=2.0.0',
 ];
